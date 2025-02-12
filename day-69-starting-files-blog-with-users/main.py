9b54a7bd945c38bdf0b062eb9cb58143e921839f
Fatkhiddin <fatkhiddinkhan@icloud.com>
2025-02-12 12:41:30 +0900

    add procfile

--- day-69-starting-files-blog-with-users/main.py
+++ day-69-starting-files-blog-with-users/main.py
@@ -1,3 +1,4 @@
+import os
 import smtplib
 import random
 from datetime import date
@@ -28,7 +29,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, EmailCheck, CommentFo
 ########################################
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("APP_SECKEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -37,7 +38,7 @@ Bootstrap5(app)
 ########################################
 SMTP_SERVER = "smtp.mail.yahoo.com"  # For Yahoo
 SENDER_EMAIL = "fatkhiddinkhan@yahoo.com"
-SENDER_PASSWORD = "ryibyziisapxetet" # <- add you app password from yahoo, or any email service
+SENDER_PASSWORD =  os.environ.get("APP_PASSWORD") # <- add you app password from yahoo, or any email service
 TO_EMAIL = None  # This will be updated dynamically
 
 ########################################
@@ -438,4 +439,4 @@ def contact():
 
 if __name__ == "__main__":
     # Run the application in debug mode for development.
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
